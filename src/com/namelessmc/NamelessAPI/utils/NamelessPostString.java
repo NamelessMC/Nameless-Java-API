49711c9f316263b00196dab8a5982b94806626ff
RobinMC <robinslot@hotmail.nl>
2017-08-07 17:06:18 +0200

    Cleanup

--- src/com/namelessmc/NamelessAPI/utils/NamelessPostString.java
+++ src/com/namelessmc/NamelessAPI/utils/NamelessPostString.java
@@ -12,24 +12,5 @@ public class NamelessPostString {
 			throw new RuntimeException(e.getMessage());
 		}
 	}
-	
-	/*public static String getPlayerPostString(UUID uuid) {
-		try {
-			return "uuid=" + URLEncoder.encode(uuid.toString(), "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}*/
-	
-	/*public static String getGroupPostString(String groupName) {
-		String string = null;
-		try {
-			string = "&group_id=" + URLEncoder.encode(groupName, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		return string;
-	}*/
 
 }
