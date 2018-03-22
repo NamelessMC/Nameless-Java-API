20a21c0f5f8a3a1a735cb28587baae861ca98c0d
Derkades <robinslot@hotmail.nl>
2018-03-22 20:58:47 +0100

    Added announcements api

---
+++ src/com/namelessmc/NamelessAPI/Announcement.java
@@ -0,0 +1,27 @@
+package com.namelessmc.NamelessAPI;
+
+public class Announcement {
+	
+	private String content;
+	private String[] displayPages;
+	private String[] displayRanks;
+	
+	public Announcement(String content, String[] displayPages, String[] displayRanks) {
+		this.content = content;
+		this.displayPages = displayPages;
+		this.displayRanks = displayRanks;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public String[] getDisplayPages() {
+		return displayPages;
+	}
+	
+	public String[] getDisplayRanks() {
+		return displayRanks;
+	}
+
+}
--- src/com/namelessmc/NamelessAPI/NamelessAPI.java
+++ src/com/namelessmc/NamelessAPI/NamelessAPI.java
@@ -3,7 +3,11 @@ package com.namelessmc.NamelessAPI;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.namelessmc.NamelessAPI.Request.Action;
 
@@ -38,5 +42,58 @@ public final class NamelessAPI {
 			throw new RuntimeException(e.getMessage());
 		}
 	}
+	
+	static String[] jsonToArray(JsonArray jsonArray) {
+		List<String> list = new ArrayList<>();
+		jsonArray.iterator().forEachRemaining((element) -> list.add(element.getAsString()));
+		return list.toArray(new String[] {});
+	}
+	
+	/**
+	 * Get all announcements
+	 * @param apiUrl
+	 * @return
+	 * @throws NamelessException
+	 */
+	public static List<Announcement> getAnnouncements(URL apiUrl) throws NamelessException {
+		Request request = new Request(apiUrl, Action.GET_ANNOUNCEMENTS);
+		
+		List<Announcement> announcements = new ArrayList<>();
+		
+		JsonObject object = request.getResponse();
+		object.getAsJsonArray().forEach((element) -> {
+			JsonObject announcementJson = element.getAsJsonObject();
+			String content = announcementJson.get("content").getAsString();
+			String[] display = jsonToArray(announcementJson.get("display").getAsJsonArray());
+			String[] permissions = jsonToArray(announcementJson.get("permissions").getAsJsonArray());
+			announcements.add(new Announcement(content, display, permissions));
+		});
+		
+		return announcements;
+	}
+	
+	/**
+	 * Get all announcements visible for the player with the specified uuid
+	 * @param apiUrl
+	 * @return
+	 * @throws NamelessException
+	 */
+	public static List<Announcement> getAnnouncements(URL apiUrl, UUID uuid) throws NamelessException {
+		Request request = new Request(apiUrl, Action.GET_ANNOUNCEMENTS, new ParameterBuilder().add("uuid", uuid).build());
+		
+		List<Announcement> announcements = new ArrayList<>();
+		
+		JsonObject object = request.getResponse();
+		object.getAsJsonArray().forEach((element) -> {
+			JsonObject announcementJson = element.getAsJsonObject();
+			String content = announcementJson.get("content").getAsString();
+			String[] display = jsonToArray(announcementJson.get("display").getAsJsonArray());
+			String[] permissions = jsonToArray(announcementJson.get("permissions").getAsJsonArray());
+			announcements.add(new Announcement(content, display, permissions));
+		});
+		
+		return announcements;
+	}
+
 
 }
