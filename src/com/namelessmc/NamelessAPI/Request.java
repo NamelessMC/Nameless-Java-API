05e1e7a77530a2ee87d8c40a6e6c89d19c8b751e
Derkades <robinslot@hotmail.nl>
2020-09-14 19:58:48 +0200

    Simplify request api

--- src/com/namelessmc/NamelessAPI/Request.java
+++ src/com/namelessmc/NamelessAPI/Request.java
@@ -3,18 +3,15 @@ package com.namelessmc.NamelessAPI;
 import static com.namelessmc.NamelessAPI.Request.RequestMethod.GET;
 import static com.namelessmc.NamelessAPI.Request.RequestMethod.POST;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.OutputStream;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
+import java.nio.charset.Charset;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -69,7 +66,6 @@ public class Request {
 	}
 
 	public void connect() throws NamelessException {
-
 		if (NamelessAPI.DEBUG_MODE) {
 			System.out.println(String.format("NamelessAPI > Making %s request (%s", this.action.toString(), this.method.toString()));
 			System.out.println(String.format("NamelessAPI > URL: %s", this.url));
@@ -77,89 +73,28 @@ public class Request {
 		}
 
 		try {
-			if (this.url.toString().startsWith("https://")){
-				final HttpsURLConnection connection = (HttpsURLConnection) this.url.openConnection();
+			final HttpURLConnection connection = (HttpURLConnection) this.url.openConnection();
 
-				connection.setRequestMethod(this.method.toString());
-				connection.setRequestProperty("Content-Length", Integer.toString(this.parameters.length()));
+			connection.setRequestMethod(this.method.toString());
+			connection.addRequestProperty("User-Agent", this.userAgent);
+			
+			
+			if (this.method == RequestMethod.POST) {
+				final byte[] encodedMessage = this.parameters.getBytes(Charset.forName("UTF-8"));
+				connection.setRequestProperty("Content-Length", encodedMessage.length + "");
 				connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-				connection.addRequestProperty("User-Agent", this.userAgent);
-
-				// If request method is POST, send parameters. Otherwise, they're already included in the URL
-				if (this.action.method == RequestMethod.POST) {
-					connection.setDoOutput(true);
-					final DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
-					final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-					writer.write(this.parameters);
-					writer.flush();
-					writer.close();
-					outputStream.close();
+				connection.setDoOutput(true);
+				try (OutputStream out = connection.getOutputStream()){
+					out.write(encodedMessage);
 				}
-
-				// Initialize input stream
-				final InputStream inputStream = connection.getInputStream();
-
-				// Handle response
-				final BufferedReader streamReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-				final StringBuilder responseBuilder = new StringBuilder();
-
-				String responseString;
-				while ((responseString = streamReader.readLine()) != null) {
-					responseBuilder.append(responseString);
-				}
-
-				final JsonParser parser = new JsonParser();
-
-				this.response = parser.parse(responseBuilder.toString()).getAsJsonObject();
-
-				inputStream.close();
-
-				// Disconnect
-				connection.disconnect();
-			} else {
-				final HttpURLConnection connection = (HttpURLConnection) this.url.openConnection();
-
-				connection.setRequestMethod(this.method.toString());
-				connection.setRequestProperty("Content-Length", Integer.toString(this.parameters.length()));
-				connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-				connection.addRequestProperty("User-Agent", this.userAgent);
-
-				// If request method is POST, send parameters. Otherwise, they're already included in the URL
-				if (this.action.method == RequestMethod.POST) {
-					connection.setDoOutput(true);
-					final DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
-					final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-					writer.write(this.parameters);
-					writer.flush();
-					writer.close();
-					outputStream.close();
-				}
-
-				// Initialize input stream
-				final InputStream inputStream = connection.getInputStream();
-
-				// Handle response
-				final BufferedReader streamReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-				final StringBuilder responseBuilder = new StringBuilder();
-
-				String responseString;
-				while ((responseString = streamReader.readLine()) != null) {
-					responseBuilder.append(responseString);
-				}
-
-				final JsonParser parser = new JsonParser();
-
-				if (NamelessAPI.DEBUG_MODE) {
-					System.out.println(String.format("NamelessAPI > Response: %s", responseBuilder.toString()));
-				}
-
-				this.response = parser.parse(responseBuilder.toString()).getAsJsonObject();
-
-				inputStream.close();
-
-				// Disconnect
-				connection.disconnect();
 			}
+
+			try (InputStream in = connection.getInputStream();
+					Reader reader = new InputStreamReader(connection.getInputStream(), Charset.forName("UTF-8"))){
+				JsonParser.parseReader(reader).getAsJsonObject();
+			}
+
+			connection.disconnect();
 
 			this.hasError = this.response.get("error").getAsBoolean();
 			if (this.hasError) {
@@ -197,15 +132,6 @@ public class Request {
 		public String toString() {
 			return this.name;
 		}
-
-		/*@Override
-		public String toString() {
-			List<String> list = Arrays.asList(super.toString().split("_"));
-			StringBuilder builder = new StringBuilder();
-			builder.append(list.remove(0).toLowerCase(Locale.ENGLISH));
-			list.forEach((element) -> builder.append(element.substring(0, 1) + element.substring(1).toLowerCase()));
-			return builder.toString();
-		}*/
 
 	}
 
