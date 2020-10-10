fc56fadc1dbb1e7f28c592afea9019372b07e75c
Derkades <robinslot@hotmail.nl>
2020-10-10 19:56:38 +0200

    apparently charset as param is java 11 only

--- src/com/namelessmc/java_api/RequestHandler.java
+++ src/com/namelessmc/java_api/RequestHandler.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -66,7 +67,11 @@ public class RequestHandler {
 					urlBuilder.append(parameters[i]);
 				} else {
 					urlBuilder.append("=");
-					urlBuilder.append(URLEncoder.encode(parameters[i].toString(), StandardCharsets.UTF_8));
+					try {
+						urlBuilder.append(URLEncoder.encode(parameters[i].toString(), StandardCharsets.UTF_8.toString()));
+					} catch (final UnsupportedEncodingException e) {
+						throw new RuntimeException(e);
+					}
 				}
 			}
 		}
