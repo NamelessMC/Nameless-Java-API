17ab1d078b6e363ab8e348a0aef7bf3a5b858569
Robin <robinslot@hotmail.nl>
2022-03-30 18:26:42 +0200

    Use modern http client

--- src/com/namelessmc/java_api/RequestHandler.java
+++ src/com/namelessmc/java_api/RequestHandler.java
@@ -2,6 +2,7 @@ package com.namelessmc.java_api;
 
 import com.google.common.base.Ascii;
 import com.google.common.base.Preconditions;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
@@ -10,12 +11,16 @@ import com.namelessmc.java_api.logger.ApiLogger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.function.Supplier;
@@ -25,13 +30,23 @@ public class RequestHandler {
 
 	private final @NotNull URL baseUrl;
 	private final @NotNull String apiKey;
+	private final @NotNull HttpClient httpClient;
 	private final @NotNull String userAgent;
 	private final @Nullable ApiLogger debugLogger;
-	private final int timeout;
+	private final @Nullable Duration timeout;
+	private final @NotNull Gson gson;
 
-	RequestHandler(final @NotNull URL baseUrl, final @NotNull String apiKey, final @NotNull String userAgent, @Nullable ApiLogger debugLogger, final int timeout) {
+	RequestHandler(final @NotNull URL baseUrl,
+				   final @NotNull String apiKey,
+				   final @NotNull HttpClient httpClient,
+				   final @NotNull Gson gson,
+				   final @NotNull String userAgent,
+				   final @Nullable ApiLogger debugLogger,
+				   final @Nullable Duration timeout) {
 		this.baseUrl = Objects.requireNonNull(baseUrl, "Base URL is null");
 		this.apiKey = Objects.requireNonNull(apiKey, "Api key is null");
+		this.httpClient = Objects.requireNonNull(httpClient, "http client is null");
+		this.gson = gson;
 		this.userAgent = Objects.requireNonNull(userAgent, "User agent is null");
 		this.debugLogger = debugLogger;
 		this.timeout = timeout;
@@ -47,15 +62,8 @@ public class RequestHandler {
 
 	public @NotNull JsonObject post(final @NotNull String route, final @Nullable JsonObject postData) throws NamelessException {
 		Preconditions.checkArgument(!route.startsWith("/"), "Route must not start with a slash");
-
-		URL url;
-		try {
-			url = new URL(this.baseUrl + "/" + route);
-		} catch (final MalformedURLException e) {
-			throw new NamelessException("Invalid URL or parameter string");
-		}
-
-		return makeConnection(url, postData);
+		URI uri = URI.create(this.baseUrl + "/" + route);
+		return makeConnection(uri, postData);
 	}
 
 	public @NotNull JsonObject get(final @NotNull String route, final @NotNull Object @NotNull... parameters) throws NamelessException {
@@ -86,14 +94,8 @@ public class RequestHandler {
 			}
 		}
 
-		final @NotNull URL url;
-		try {
-			url = new URL(urlBuilder.toString());
-		} catch (final MalformedURLException e) {
-			throw new NamelessException("Error while building request URL: " + urlBuilder, e);
-		}
-
-		return makeConnection(url, null);
+		final @NotNull URI uri = URI.create(urlBuilder.toString());
+		return makeConnection(uri, null);
 	}
 
 	private void debug(final @NotNull String message, @NotNull Supplier<Object[]> argsSupplier) {
@@ -102,47 +104,37 @@ public class RequestHandler {
 		}
 	}
 
-	private @NotNull JsonObject makeConnection(final URL url, final @Nullable JsonObject postBody) throws NamelessException {
-		final HttpURLConnection connection;
-		final byte[] bytes;
+	private @NotNull JsonObject makeConnection(final URI uri, final @Nullable JsonObject postBody) throws NamelessException {
+		HttpRequest.Builder reqBuilder = HttpRequest.newBuilder(uri);
+		if (timeout != null) {
+			reqBuilder.timeout(timeout);
+		}
+
+		debug("Making connection %s to url %s", () -> new Object[]{ postBody != null ? "POST" : "GET", uri});
+
+		if (postBody != null) {
+			byte[] postBytes = gson.toJson(postBody).getBytes(StandardCharsets.UTF_8);
+			reqBuilder.POST(HttpRequest.BodyPublishers.ofByteArray(postBytes));
+			reqBuilder.header("Content-Type", "application/json");
+
+			debug("Post body below\n-----------------\n%s\n-----------------",
+					() -> new Object[] { new String(postBytes, StandardCharsets.UTF_8) });
+		} else {
+			reqBuilder.GET();
+		}
+
+		reqBuilder.header("User-Agent", this.userAgent);
+		reqBuilder.header("X-API-Key", this.apiKey);
+
+		HttpRequest httpRequest = reqBuilder.build();
+
+		int statusCode;
+		String responseBody;
 		try {
-			connection = (HttpURLConnection) url.openConnection();
-
-			connection.setReadTimeout(this.timeout);
-			connection.setConnectTimeout(this.timeout);
-
-			debug("Making connection %s to url %s", () -> new Object[]{ postBody != null ? "POST" : "GET", url});
-
-			connection.addRequestProperty("User-Agent", this.userAgent);
-			connection.addRequestProperty("X-API-Key", this.apiKey);
-
-			debug("Using User-Agent '%s'", () -> new Object[]{ this.userAgent });
-
-			if (postBody != null) {
-				debug("Post body below\n-----------------\n%s\n-----------------", () -> new Object[] { postBody });
-				connection.setRequestMethod("POST");
-				final byte[] encodedMessage = postBody.toString().getBytes(StandardCharsets.UTF_8);
-				connection.setRequestProperty("Content-Length", encodedMessage.length + "");
-				connection.setRequestProperty("Content-Type", "application/json");
-				connection.setDoOutput(true);
-				try (OutputStream out = connection.getOutputStream()) {
-					out.write(encodedMessage);
-				}
-			}
-
-			if (connection.getResponseCode() >= 400) {
-				try (final InputStream in = connection.getErrorStream()) {
-					if (in == null) {
-						throw new NamelessException("Website sent empty response with code " + connection.getResponseCode());
-					} else {
-						bytes = getBytesFromInputStream(in);
-					}
-				}
-			} else {
-				try (final InputStream in = connection.getInputStream()) {
-					bytes = getBytesFromInputStream(in);
-				}
-			}
+			HttpResponse<String> httpResponse = httpClient.send(httpRequest,
+					HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+			statusCode = httpResponse.statusCode();
+			responseBody = httpResponse.body();
 		} catch (final IOException e) {
 			final StringBuilder message = new StringBuilder("Network connection error (not a Nameless issue).");
 			if (e.getMessage().contains("unable to find valid certification path to requested target")) {
@@ -151,49 +143,48 @@ public class RequestHandler {
 			message.append(" IOException: ");
 			message.append(e.getMessage());
 			throw new NamelessException(message.toString(), e);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
 		}
 
-		final String response = new String(bytes, StandardCharsets.UTF_8);
+		debug("Website response below\n-----------------\n%s\n-----------------",
+				() -> new Object[] { regularAsciiOnly(responseBody) });
 
-		if (response.equals("API is disabled")) {
+		if (responseBody.length() == 0) {
+			throw new NamelessException("Website sent empty response with status code " + statusCode);
+		}
+
+		if (responseBody.equals("API is disabled")) {
 			throw new ApiDisabledException();
 		}
-
-		debug("Website response below\n-----------------\n%s\n-----------------", () -> new Object[] { regularAsciiOnly(response) });
 
 		JsonObject json;
 
 		try {
-			json = JsonParser.parseString(response).getAsJsonObject();
+			json = JsonParser.parseString(responseBody).getAsJsonObject();
 		} catch (final JsonSyntaxException | IllegalStateException e) {
-			int code;
-			try {
-				code = connection.getResponseCode();
-			} catch (final IOException e1) {
-				throw new IllegalStateException("We've already made a connection, getting an IOException now is impossible", e1);
-			}
-
 			StringBuilder message = new StringBuilder();
 			message.append("Website returned invalid response with code ");
-			message.append(code);
+			message.append(statusCode);
 			message.append(".\n");
-			if (code == 301 || code == 302 || code == 303) {
+			if (statusCode >= 301 && statusCode <= 303) {
 				message.append("HINT: The URL results in a redirect. If your URL uses http://, change to https://. If your website forces www., make sure to add www. to the url.\n");
-			} else if (code == 520 || code == 521) {
+			} else if (statusCode == 520 || statusCode == 521) {
 				message.append("HINT: Status code 520/521 is sent by CloudFlare when the backend webserver is down or having issues.\n");
-			} else if (response.contains("/aes.js")) {
+			} else if (responseBody.contains("/aes.js")) {
 				message.append("HINT: It looks like requests are being blocked by your web server or a proxy. ");
 				message.append("This is a common occurrence with free web hosting services; they usually don't allow API access.\n");
-			} else if (response.contains("<title>Please Wait... | Cloudflare</title>")) {
+			} else if (responseBody.contains("<title>Please Wait... | Cloudflare</title>")) {
 				message.append("HINT: CloudFlare is blocking our request. Please see https://docs.namelessmc.com/cloudflare-apis\n");
-			} else if (response.startsWith("\ufeff")) {
+			} else if (responseBody.startsWith("\ufeff")) {
 				message.append("HINT: The website response contains invisible unicode characters.\n");
 			}
 
 			message.append("Website response:\n");
 			message.append("-----------------\n");
 			int totalLengthLimit = 1950; // fit in a Discord message with safety margin
-			message.append(Ascii.truncate(regularAsciiOnly(response), totalLengthLimit - message.length(), "[truncated]\n"));
+			String printableResponse = regularAsciiOnly(responseBody);
+			message.append(Ascii.truncate(printableResponse, totalLengthLimit - printableResponse.length(), "[truncated]\n"));
 			if (message.charAt(message.length()) != '\n') {
 				message.append('\n');
 			}
@@ -214,15 +205,6 @@ public class RequestHandler {
 		}
 
 		return json;
-	}
-
-	private static byte @NotNull[] getBytesFromInputStream(final @NotNull InputStream is) throws IOException {
-	    final ByteArrayOutputStream os = new ByteArrayOutputStream();
-	    final byte[] buffer = new byte[0xFFFF];
-	    for (int len = is.read(buffer); len != -1; len = is.read(buffer)) {
-	        os.write(buffer, 0, len);
-	    }
-	    return os.toByteArray();
 	}
 
 	private static @NotNull String regularAsciiOnly(@NotNull String message) {
