d44e29d74467e49f66280990a9953e86284caffa
Robin <robin+git@rkslot.nl>
2022-08-01 14:23:26 +0200

    Add methods to create order

--- src/com/namelessmc/java_api/modules/store/StoreUser.java
+++ src/com/namelessmc/java_api/modules/store/StoreUser.java
@@ -1,10 +1,13 @@
 package com.namelessmc.java_api.modules.store;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.namelessmc.java_api.exception.NamelessException;
 import com.namelessmc.java_api.NamelessUser;
 import com.namelessmc.java_api.RequestHandler;
 import com.namelessmc.java_api.modules.ModuleNames;
+
+import java.util.List;
 
 public class StoreUser {
 
@@ -50,6 +53,23 @@ public class StoreUser {
 	public int creditsCents() throws NamelessException {
 		JsonObject response = this.requests.get("users/" + this.user.userTransformer() + "/credits");
 		return response.get("cents").getAsInt();
+	}
+
+	public void createOrder(StoreCustomer purchaser, StoreCustomer recipient, List<StoreProduct> products) throws NamelessException {
+		JsonArray productIds = new JsonArray(products.size());
+		for (int i = 0; i < products.size(); i++) {
+			productIds.add(products.get(i).id());
+		}
+		this.createOrder(purchaser.id(), recipient.id(), productIds);
+	}
+
+	public void createOrder(int purchaserCustomerId, int recipientCustomerId, JsonArray productIds) throws NamelessException {
+		JsonObject body = new JsonObject();
+		body.addProperty("user", this.user.userTransformer());
+		body.addProperty("customer", purchaserCustomerId);
+		body.addProperty("recipient", recipientCustomerId);
+		body.add("products", productIds);
+		this.requests.post("store/order/create", body);
 	}
 
 }
